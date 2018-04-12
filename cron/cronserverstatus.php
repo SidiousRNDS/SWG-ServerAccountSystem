1c32fd7ec30bda4cf73109db4d5db67224fcfc05
sidiousRNDS <sidious.rnds.io>
2018-04-11 22:03:58 -0700

    Updated cron script added absolute paths to fix the issue with cron not running

--- cron/cronserverstatus.php
+++ cron/cronserverstatus.php
@@ -10,13 +10,13 @@
      * your servers
      ******************************************************************/
     
-    require '../vendor/autoload.php';
+    require dirname(dirname(__FILE__)).'/vendor/autoload.php';
     
     // Use swgAS
     use \swgAS\config\settings;
     use \swgAS\swgStatus\models\statusModel;
     
-    require '../app/config/di.php';
+    require dirname(dirname(__FILE__)).'/app/config/di.php';
     
     statusModel::getServerStatus(['mongodb'=>$ci['mongodb']]);
     
