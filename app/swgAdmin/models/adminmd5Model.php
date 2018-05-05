f7935454ca7fe32c4d1ba408e1d085af066805e2
Sidious <sidious@rnds.io>
2018-05-04 18:34:36 -0700

    Broke out some of the functionality for the md5 loader as well as setting up a model for the md5 collection

---
+++ app/swgAdmin/models/adminmd5Model.php
@@ -0,0 +1,52 @@
+<?php
+/*****************************************************************
+ * RNDS SWG Account System
+ * @author: Sidious <sidious@rnds.io>
+ * @since: 04 May 2018
+ * @link: https://github.com/SidiousRNDS/SWGRO-AccountSystem
+ * @version: 1.0.0
+ ******************************************************************
+ * NAMESPACE: swgAS\swgAdmin\models
+ * CLASS: adminmd5Model
+ ******************************************************************/
+
+namespace swgAS\swgAdmin\models;
+
+// Use
+use \MongoDB\Driver\BulkWrite as MongoBulkWrite;
+use \MongoDB\BSON\ObjectId as MongoID;
+
+// Use swgAS
+use swgAS\config\settings;
+
+class adminmd5Model
+{
+    private $md5Collection = "tre_md5";
+
+    /**
+     * @param $args
+     * @return mixed
+     */
+    public function loadMD5Data($args)
+    {
+        $md5File = json_decode($args['md5Data'], true);
+        $createMD5TreBulk = new MongoBulkWrite(['ordered' => true]);
+
+        // Remove Existing Entries
+        $createMD5TreBulk->delete([]);
+
+        $x=1;
+        foreach($md5File as $index => $value)
+        {
+            if(is_array($value) && $value['md5'] != "") {
+                $createMD5TreBulk->insert(['_id' => new MongoID, 'md5' => $value['md5'], 'trefile' => $value['trefile']]);
+                print $x. ". Adding entry for " . $value['trefile'] . "\n";
+            }
+            $x++;
+        }
+
+        $loaded = $args['mongodb']->executeBulkWrite(settings::MONGO_ADMIN . "." . $this->md5Collection, $createMD5TreBulk);
+
+        return count($createMD5TreBulk);
+    }
+}
--- app/swgLoad/loadstarterdataModel.php
+++ app/swgLoad/loadstarterdataModel.php
@@ -12,38 +12,16 @@
 
 namespace swgAS\swgLoad;
 
-// Use
-use \MongoDB\Driver\BulkWrite as MongoBulkWrite;
-use \MongoDB\BSON\ObjectId as MongoID;
+use swgAS\swgAdmin\models\adminmd5Model;
 
-// Use swgAS
-use swgAS\config\settings;
 
 class loadstarterdataModel
 {
-    private static $md5Collection = "tre_md5";
-
     public static function initloadTreMD5($args)
     {
-        $md5File = json_decode($args['md5Data'], true);
-        $mongo = $args['mongodb'];
+        $md5Model = new adminmd5Model();
 
-        $createMD5TreBulk = new MongoBulkWrite(['ordered' => true]);
-        // Remove Existing Entries
-        $createMD5TreBulk->delete([]);
-
-        $x=1;
-        foreach($md5File as $index => $value)
-        {
-            if(is_array($value) && $value['md5'] != "") {
-                $createMD5TreBulk->insert(['_id' => new MongoID, 'md5' => $value['md5'], 'trefile' => $value['trefile']]);
-                print $x. ". Adding entry for " . $value['trefile'] . "\n";
-            }
-            $x++;
-        }
-
-        $args['mongodb']->executeBulkWrite(settings::MONGO_ADMIN . "." . static::$md5Collection, $createMD5TreBulk);
-
-        return [count($createMD5TreBulk)-1, count($md5File)];
+        $md5loaded = $md5Model->loadMD5Data($args);
+        return [$md5loaded-1, count(json_decode($args['md5Data'], true))];
     }
 }
